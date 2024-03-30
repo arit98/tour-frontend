24598b31a302e050647535429bae90d3aa717e5b
arit98 <aritradaschowdhury@gmail.com>
2024-03-30 16:39:19 +0530

    test 1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
-// export const BASE_URL = "https://tour-api-5cpx.onrender.com/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "https://tour-api-production-e7fc.up.railway.app/api/v1";
